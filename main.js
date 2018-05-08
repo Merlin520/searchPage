10be12e27fa3d1b6dbc9a403eb280b83bfff76f8
meiya <meiya90hou@163.com>
2018-05-08 22:32:51 +0800

    数据结构

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var keys = {
+    0:['q','w','e','r','t','y','u','i','o','p'],
+    1:['q','w','e','r','t','y','u','i','o','p'],
+    2:['q','w','e','r','t','y','u','i','o','p'],
+    length:3
+};
+
+var hash = {
+    q:'qq.com',
+    w:'weibo.com',
+    b:'baidu.com'
+};
+
+div1 = document.createElement('div');
+zhangsan.appendChild(div1);
