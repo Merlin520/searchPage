f3fc6d2b3ca76a70226a1c2a2b78bdb462e5e56c
meiya <meiya90hou@163.com>
2018-05-09 22:39:10 +0800

    生成div

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 var keys = {
     0:['q','w','e','r','t','y','u','i','o','p'],
-    1:['q','w','e','r','t','y','u','i','o','p'],
-    2:['q','w','e','r','t','y','u','i','o','p'],
+    1:['q','w','e','r','t','y','u','i','o'],
+    2:['q','w','e','r','t','y'],
     length:3
 };
 
@@ -12,5 +12,19 @@ var hash = {
     b:'baidu.com'
 };
 
-div1 = document.createElement('div');
-zhangsan.appendChild(div1);
+
+
+index = 0;
+while(index < keys['length']){
+    Y = document.createElement('div');
+    X.appendChild(Y);
+    row = keys[index];
+    index2 = 0;
+    while (index2 < row['length']){
+        Z = document.createElement('kbd');
+        Z.textContent = row[index2];
+        Y.appendChild(Z);
+        index2 ++ ;
+    }
+    index = index + 1;
+}
