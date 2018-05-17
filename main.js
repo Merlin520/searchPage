a2eb783d6e9f73db309dca6e8b3e453757fa302c
meiya <meiya90hou@163.com>
2018-05-17 22:43:27 +0800

    var声明

--- main.js
+++ main.js
@@ -17,25 +17,25 @@ if(hashInLocalStorage){
     hash = hashInLocalStorage;
 }
 
-index = 0;
+var index = 0;
 while(index < keys['length']){
-    div = document.createElement('div');
+    var div = document.createElement('div');
     div.className = 'row';
     main.appendChild(div);
-    row = keys[index];
-    index2 = 0;
+    var row = keys[index];
+    var index2 = 0;
     while (index2 < row['length']){
-        kbd = document.createElement('kbd');
-        span = document.createElement('span');
+        var kbd = document.createElement('kbd');
+        var span = document.createElement('span');
         span.textContent = row[index2];
         kbd.appendChild(span);
         kbd.className = 'key';
         span.className = 'text';
-        button = document.createElement('button');
+        var button = document.createElement('button');
         kbd.appendChild(button);
         button.textContent = '编辑';
         button.id = row[index2];//button是个容器
-        img = document.createElement('img');
+        var img = document.createElement('img');
         if(hash[row[index2]]){
             img.src = 'http://'+hash[row[index2]]+'/favicon.ico';
         }else {
@@ -47,10 +47,10 @@ while(index < keys['length']){
         };
 
         button.onclick = function (Q) {
-            button2 = Q['target'];
-            img2 = button2.previousSibling
-            o = Q.target.id;
-            p = prompt('给我一个网址');
+            var button2 = Q['target'];
+            var img2 = button2.previousSibling;
+            var o = button2['id'];
+            var p = prompt('给我一个网址');
              hash[o] = p ;
              img2.src = 'http://'+p+'/favicon.ico';
              img2.onerror = function (xxx) {
@@ -67,8 +67,8 @@ while(index < keys['length']){
 }
 
 document.onkeypress = function (listener) {
-    key = listener['key'];
-    website = hash[key];
+    var key = listener['key'];
+    var website = hash[key];
     // location.href = 'http://'+website
     window.open('http://'+website,'_blank')
 };
