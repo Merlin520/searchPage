01d5db254a1c7007be3d444272292a719522ac72
meiya <meiya90hou@163.com>
2018-05-21 23:03:29 +0800

    代码优化

--- main.js
+++ main.js
@@ -1,74 +1,135 @@
 
-var keys = {
-    0:['q','w','e','r','t','y','u','i','o','p'],
-    1:['q','w','e','r','t','y','u','i','o'],
-    2:['q','w','e','r','t','y'],
-    length:3
-};
+//1.初始化数据
+    var hashA = init();
+    var keys = hashA['keys'];
+    var hash = hashA['hash'];
 
-var hash = {
-    q:'qq.com',
-    w:'weibo.com',
-    b:'baidu.com'
-};
 
-var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null');
-if(hashInLocalStorage){
-    hash = hashInLocalStorage;
+
+
+
+//2.生成键盘
+generateKeyboard(keys,hash);
+
+
+
+
+//3.监听用户（键盘）
+ListenToUser(hash);
+
+
+
+
+//工具函数
+function getFormLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name) || 'null')
 }
 
-var index = 0;
-while(index < keys['length']){
-    var div = document.createElement('div');
-    div.className = 'row';
-    main.appendChild(div);
-    var row = keys[index];
-    var index2 = 0;
-    while (index2 < row['length']){
-        var kbd = document.createElement('kbd');
-        var span = document.createElement('span');
-        span.textContent = row[index2];
-        kbd.appendChild(span);
-        kbd.className = 'key';
-        span.className = 'text';
-        var button = document.createElement('button');
-        kbd.appendChild(button);
-        button.textContent = '编辑';
-        button.id = row[index2];//button是个容器
-        var img = document.createElement('img');
-        if(hash[row[index2]]){
-            img.src = 'http://'+hash[row[index2]]+'/favicon.ico';
-        }else {
-            img.src = './qq.jpg'
-        }
+function tag(tagName) {
+    return document.createElement(tagName);
+}
 
-        img.onerror = function (xxx) {
+function createSpan(textContent) {
+    var span = tag('span');
+    span.textContent = textContent;
+    span.className = 'text';
+    return span
+}
+
+function createButton(id) {
+    var button = tag('button');
+    // kbd.appendChild(button);
+    button.textContent = '编辑';
+    button.id = id;//button是个容器
+    button.onclick = function (Q) {
+        var button2 = Q['target'];
+        var img2 = button2.previousSibling;
+        var o = button2['id'];
+        var p = prompt('给我一个网址');
+        hash[o] = p ;
+        img2.src = 'http://'+p+'/favicon.ico';
+        img2.onerror = function (xxx) {
             xxx.target.src = './qq.jpg'
         };
-
-        button.onclick = function (Q) {
-            var button2 = Q['target'];
-            var img2 = button2.previousSibling;
-            var o = button2['id'];
-            var p = prompt('给我一个网址');
-             hash[o] = p ;
-             img2.src = 'http://'+p+'/favicon.ico';
-             img2.onerror = function (xxx) {
-                xxx.target.src = './qq.jpg'
-            };
-            localStorage.setItem('zzz',JSON.stringify(hash))
-        };
-        kbd.appendChild(img);
-        kbd.appendChild(button);
-        div.appendChild(kbd);
-        index2 ++ ;
-    }
-    index = index + 1;
+        localStorage.setItem('zzz',JSON.stringify(hash))
+    };
+    return button
 }
 
-document.onkeypress = function (listener) {
-    var key = listener['key'];
-    var website = hash[key];
-    // location.href = 'http://'+website
-    window.open('http://'+website,'_blank')
-};
+function createImage(domain) {
+    var img = tag('img');
+    if(domain){
+        img.src = 'http://'+domain+'/favicon.ico';
+    }else {
+        img.src = './qq.jpg'
+    }
+    img.onerror = function (xxx) {
+        xxx.target.src = './qq.jpg'
+    };
+    return img
+}
+
+function init() {
+    var keys = {
+        0:['q','w','e','r','t','y','u','i','o','p'],
+        1:['q','w','e','r','t','y','u','i','o'],
+        2:['q','w','e','r','t','y'],
+        length:3
+    };
+
+    var hash = {
+        q:'qq.com',
+        w:'weibo.com',
+        b:'baidu.com'
+    };
+
+    var hashInLocalStorage = getFormLocalStorage('zzz');
+    if(hashInLocalStorage){
+        hash = hashInLocalStorage;
+    }
+    return{
+        "keys":keys,
+        "hash":hash
+    }
+
+}
+
+function generateKeyboard(keys,hash) {
+    for(var index = 0;index < keys['length'];index++){
+        var div = tag('div');
+        div.className = 'row';
+
+        main.appendChild(div);
+
+        var row = keys[index];
+        for(index2 = 0;index2 < row['length'];index2 ++ ){
+            var span = createSpan(row[index2]);
+
+
+            var button = createButton(row[index2]);
+
+
+
+            var img = createImage(hash[row[index2]]);
+
+
+
+            var kbd = tag('kbd');
+            kbd.className = 'key';
+
+            kbd.appendChild(span);
+            kbd.appendChild(img);
+            kbd.appendChild(button);
+            div.appendChild(kbd);
+        }
+    }
+}
+
+function ListenToUser(hash) {
+    document.onkeypress = function (listener) {
+        var key = listener['key'];
+        var website = hash[key];
+        // location.href = 'http://'+website
+        window.open('http://'+website,'_blank')
+    };
+}
